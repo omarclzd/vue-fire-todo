3188f65a288ee0485612a676aaf2f5a6112f4373
omar <omasrt@gmail.com>
2019-07-31 18:25:44 -0500

    config store

--- src/store.js
+++ src/store.js
@@ -5,7 +5,31 @@ import { db } from "@/main";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {}
+  state: {
+    items: null
+  },
+  getters: {
+    getItems: state => {
+      return state.items;
+    }
+  },
+  mutations: {
+    setItems: state => {
+      let items = [];
+      db.collection("items")
+        .orderBy("created_at")
+        .onSnapshot(snapshot => {
+          items = [];
+          snapshot.forEach(doc => {
+            items.push({ id: doc.id, title: doc.data().title });
+          });
+          state.items = items;
+        });
+    }
+  },
+  actions: {
+    setItems: context => {
+      context.commit("setItems");
+    }
+  }
 });
